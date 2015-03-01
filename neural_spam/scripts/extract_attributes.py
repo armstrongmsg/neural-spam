0a8490f2108cabaa10b544fabd639d887a756639
lordivel <aandersonthelles@gmail.com>
2015-03-01 10:27:39 -0300

    Update extract_attributes.py
    criei a função de treinamento baseado em dois atributos, tamanho e de palavras chaves... a lista de palavras está incompleta... fiquei um pouco com preguiça mas, vou arrumar!
    
    Tem coisas que deveriam ser em outro arquivo, pois são valores que vão ficar modificando, depois alguém dá uma olhada e tenta fazer isso! :D

--- neural_spam/scripts/extract_attributes.py
+++ neural_spam/scripts/extract_attributes.py
@@ -3,11 +3,50 @@
 import sys
 import os.path
 
+##
+## ATTENCION: USE COMMENTS! THIS IS NOT JAVA! LET EVERYONE UNDERSTAND WHAT THE FUCK ARE YOU DOING!
+## WORK EASIER, MAKE EVERYONE HAPPY: DO DOCUMENTATION!
+##
+
 # constants
 INDEX_INPUT_FILE_PATH = 1
 INDEX_IS_SPAM = 2
-IS_SPAM = "1"
-IS_NOT_SPAM = "0"
+IS_SPAM = 1
+IS_NOT_SPAM = -1
+LEARNIG_RATE = 1
+THRESHOLD = 0 #it might be in other file, because, this value will change on neural network trainning
+
+# SPANS TRIGGER WORDS
+# From website: http://blog.hubspot.com/blog/tabid/6307/bid/30684/The-Ultimate-List-of-Email-SPAM-Trigger-Words.aspx
+AVOID_SPAM_WORDS_COMMERCE = ["as seen on","buy","buy direct","buying judgments","clearance","order","order status","orders shipped by","shopper"]
+AVOID_SPAM_WORDS_PERSONAL = ["dig up dirt on friends","meet singles","score with babes"]
+AVOID_SPAM_WORDS_EMPLOYMENTS = ["additional income","be your own boss","compete for your business","double your","earn $","earn extra cash","earn per week","expect to earn","extra income","home based","home employment","homebased business","income from home","make $","make money","money making","online biz opportunity","online degree","opportunity","potential earnings","university diplomas","while you sleep","work at home","work from home"]
+AVOID_SPAM_WORDS_FINANCIAL_GENERAL = []
+AVOID_SPAM_WORDS_FINANCIAL_BUSINESS = []
+AVOID_SPAM_WORDS_FINANCIAL_PERSONAL = []
+AVOID_SPAM_WORDS_GENERAL = []
+AVOID_SPAM_WORDS_GREETINGS = []
+AVOID_SPAM_WORDS_MARKETING = []
+AVOID_SPAM_WORDS_MEDICAL = []
+AVOID_SPAM_WORDS_NUMBERS = []
+AVOID_SPAM_WORDS_OFFERS = []
+AVOID_SPAM_WORDS_CALL_TO_ACTION = []
+AVOID_SPAM_WORDS_FREE = []
+AVOID_SPAM_WORDS_DESCRIPTION = []
+AVOID_SPAM_WORDS_SENSE_OF_URGENCY = []
+AVOID_SPAM_WORDS_NOUNS = []
+
+SPAM_TRIGGER_WORDS = [AVOID_SPAM_WORDS_COMMERCE,AVOID_SPAM_WORDS_PERSONAL,AVOID_SPAM_WORDS_EMPLOYMENTS,AVOID_SPAM_WORDS_FINANCIAL_GENERAL,AVOID_SPAM_WORDS_FINANCIAL_BUSINESS,AVOID_SPAM_WORDS_FINANCIAL_PERSONAL,AVOID_SPAM_WORDS_GENERAL,AVOID_SPAM_WORDS_GREETINGS,AVOID_SPAM_WORDS_MARKETING,AVOID_SPAM_WORDS_MEDICAL,AVOID_SPAM_WORDS_NUMBERS,AVOID_SPAM_WORDS_OFFERS,AVOID_SPAM_WORDS_CALL_TO_ACTION,AVOID_SPAM_WORDS_FREE,AVOID_SPAM_WORDS_DESCRIPTION,AVOID_SPAM_WORDS_SENSE_OF_URGENCY,AVOID_SPAM_WORDS_NOUNS]
+
+
+
+#it might be in other file, because, these values will change on neural network trainning
+# it might be an array or class
+WEI_SUBJECT_LENGTH = 1
+WEI_SUBJECT_TRIGGER_WORDS = 1
+
+
+#
 
 class EmailAttributes:
 	def __init__(self, subject):
@@ -18,6 +57,19 @@ class EmailAttributes:
 
 	def __str__(self):
 		return "\"" + self.subject + "\""
+
+# class used to neural network trainning
+class Email:
+	# attributes: instance of EmailAttributes
+	# spam: boolean if is a spam or a ham
+	def __init__(self, attributes, spam):
+		self.attributes = attributes
+		self.spam = spam
+	def get_attributes(self):
+		return attributes
+	def get_spam(self):
+		return spam
+
 
 def args_treatment(args):
 	if len(args) != 3:
@@ -44,6 +96,48 @@ def extract_attributes(file, is_spam):
 	# removing spaces
 	subject = subject.strip()
 	return EmailAttributes(subject)
+
+#Emails: Email Array
+def learning(emails):
+	for (email in emails):
+		while (True):
+			#Checking parameters
+			#it might be a while or for here, checking all attributes and weights, but, I don't know how to do it now...
+			# w1,w2,w3... are local weigths
+			w1 = len(email.get_attribute().get_subject()) * WEI_SUBJECT_LENGTH
+			avoid = 0;
+			for (avoid_word in SPAM_TRIGGER_WORDS):
+				if (email.get_attribute().get_subject().lower() in avoid_word):
+					avoid = 1;
+			w2 = avoid * WEI_SUBJECT_TRIGGER_WORDS
+			
+			# sum all weights
+			w = w1+w2
+			
+			#check threshold
+			espected = 0
+			descovered = 0
+			if (w > THRESHOLD):
+				if(email.get_spam()):
+					break
+				else:
+					espected = IS_SPAM
+					descovered = IS_NOT_SPAM
+			else:
+				if not(email.get_spam()):
+					break
+				else:
+					espected = IS_NOT_SPAM
+					descovered = IS_SPAM
+			
+			# weight adjustment
+			# do not change
+			WEI_SUBJECT_LENGTH = WEI_SUBJECT_LENGTH + ((LEARNIG_RATE*len(email.get_attribute().get_subject()))*(espected-descovered))
+			WEI_SUBJECT_TRIGGER_WORDS = WEI_SUBJECT_TRIGGER_WORDS + ((LEARNING_RATE*avoid)*(espected-descovered))
+			THRESHOLD = THRESHOLD + ((LEARNING_RATE*(-1))*(espected-descovered))
+		
+
+
 
 args = sys.argv
 args_treatment(args)
